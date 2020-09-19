b9f1fb7cbbb7606386ab657759bef40e9e8be3be
thanhtutwin <steven.w.thanhtut@gmail.com>
2020-09-19 16:49:54 +0630

    test express

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // create an express app
 const express = require("express");
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 
 // use the express-static middleware
 app.use(express.static("public"));
 
+app.use(bodyParser.json());
+
 // define the first route
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>");
 })
+
+
+
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
