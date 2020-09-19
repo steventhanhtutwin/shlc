131ea2a3e819e864566ae3ae95e16aa797ba793b
thanhtutwin <steven.w.thanhtut@gmail.com>
2020-09-19 16:35:05 +0630

    test express

--- app.js
+++ app.js
@@ -1,58 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-var app = express();
+// create an express app
+const express = require("express")
+const app = express()
 
-var port = process.env.PORT || 8000;
+// use the express-static middleware
+app.use(express.static("public"))
 
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
 })
 
-app.get('/user', (req, res) => {
-
-   var json = JSON.stringify(req.body);
-
-   console.log(json);
-
-   var getdata = JSON.parse(json);
-
-   //console.log(req.body);
-
-    var connection = mysql.createConnection({
-        user :'b786db4142dff0',
-        password : 'e7c35856',
-        host:'us-cdbr-east-02.cleardb.com',
-        database:'heroku_2fa387dfa408f94'
-    })
-  
-    var username = getdata.username;
-    var password = getdata.password;
-    var email = getdata.email ;
-    var phonenumber = getdata.phonenumber;
-    var address = getdata.address ;
-    var selectpainting = getdata.selectpainting;
-
-    connection.connect()
-
-    connection.query("INSERT INTO heroku_2fa387dfa408f94.shlcusers(username, password, email, phonenumber, address, selectpanting) values ('"+ username +"','"+ password +"','"+ email + "','"+ phonenumber + "','"+ address + "','"+ selectpainting + "')", function (err, rows, fields) {
-       console.log('The solution is inserted');
-    })
-
-    console.log("Connect");
-    
-    connection.end();
-
-    //res.send("Thanks");
-    res.sendStatus(200);
-    res.end();
-})
-
-
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
-
-
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
