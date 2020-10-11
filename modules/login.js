042e94499d1f4b03487b470a814c147abcfd1a36
shlconlinelearning <tech.shlc@gmail.com>
2020-10-11 11:33:39 +0630

    commit all files

--- modules/login.js
+++ modules/login.js
@@ -27,9 +27,20 @@ router.post('/login', (req, res) => {
     
     var username = req.body.username;
     var password = req.body.password;
+    var courseid = req.body.courseid;
 
     console.log(username);
     console.log(password);
+    console.log(courseid);
+
+    if(courseid > 0)
+    {
+        console.log('>0');
+    } 
+    else
+    {
+        console.log('=0');
+    }
  
      connection.connect();
 
@@ -47,6 +58,26 @@ router.post('/login', (req, res) => {
         }
 
         connection.end();
+
+
+        var connections = mysql.createConnection({
+            user :'b44f084e2826b8',
+            password : '21165fd2',
+            host:'us-cdbr-east-02.cleardb.com',
+            database:'heroku_8e74fc53b2ed17d'
+        })
+        
+    
+         connections.connect();
+     
+         connections.query("INSERT INTO heroku_8e74fc53b2ed17d.enrollment(useremail,CourseId,RequestedOn,status) values ('"+ username +"', '"+ courseid +"', CURRENT_TIMESTAMP(), '0' )", function (err, rows, fields) {
+            console.log('The solution is inserted');
+         })
+     
+         console.log("Connect enrollment");
+         
+         connections.end();
+
 
         console.log(rowsrecord.length);
 
