b4aead507afb5413407c66123cae31dc0d263c1c
shlconlinelearning <tech.shlc@gmail.com>
2020-10-27 20:46:18 +0630

    fix the send email issues

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const timeout = require('connect-timeout');
 var register = require('./modules/register');
 var login = require('./modules/login');
 var forgotpassword = require('./modules/forgotpassword');
@@ -16,12 +17,22 @@ var updateuserdetails = require('./modules/updateuserdetails');
 
 const app = express();
 
+const haltOnTimedout = (req, res, next) => {
+  if (!req.timedout) {
+    next();
+  }
+}
+
+app.use(timeout('5s'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(haltOnTimedout)
+
 app.use(cors());
 
 // use the express-static middleware
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
 
 // define the first route
@@ -50,6 +61,7 @@ app.use('/user',login);
 
 // Global error handler - route handlers/middlewares which throw end up here
 app.use((err, req, res, next) => {
+
   // response to user with 403 error and details
   console.error(err.stack);
 
--- modules/enrollment.js
+++ modules/enrollment.js
@@ -2,7 +2,6 @@ const express = require("express");
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const sendEmail = require('../resources/enrollcourse');
 
 const router = express.Router();
 
@@ -14,15 +13,18 @@ router.use(express.static("public"));
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+
 router.post('/enrollment', (req, res) => {
+
 
     var rowsrecord;
  
-    var connection = mysql.createConnection({
+    var connection = mysql.createPool({
         user :'b44f084e2826b8',
         password : '21165fd2',
         host:'us-cdbr-east-02.cleardb.com',
-        database:'heroku_8e74fc53b2ed17d'
+        database:'heroku_8e74fc53b2ed17d',
+        connectionLimit : 10
     })
     
     var email = req.body.email;
@@ -31,51 +33,20 @@ router.post('/enrollment', (req, res) => {
 
     console.log("Calling enrollment!");
 
-    connection.connect();
+    
+    connection.query("SELECT * FROM heroku_8e74fc53b2ed17d.enrollment where status > 0 and useremail = '"+ email + "' ;", function (err, rows, fields) {
+        console.log("Enrollment");
+        rowsrecord = rows;
 
-     connection.query("SELECT * FROM heroku_8e74fc53b2ed17d.enrollment where status > 0 and useremail = '"+ email + "' ;", function (err, rows, fields) {
-        if (err)
-        { 
-            res.status(400).json({
-                status: 'fail',
-                errmessage: 'user email format is not correct'
-            });
-
-            res.end();
-        }
-        else
-        {
-            //console.log(rows);
-            // rowsrecord = rows.recordsets[0];
-            rowsrecord = rows;
-           // console.log(lng);
-        }
-
-        connection.end();
-     
-       // console.log(rowsrecord.length);
-
-        if (rowsrecord.length > 0)
-        {
-            console.log("Found!");
-            res.status(200).json({
-                data:rowsrecord,
-                status: 'success'
-            });
-        }
-        else
-        {
-            console.log("Not Found!");
-
-            res.status(300).json({
-                data:rowsrecord,
-                status: 'fail'
-             });     
-        }
-
+        res.status(200).json({
+            data:rowsrecord,
+            status: 'success'
+        });
+    
         res.end();
+    });  
+});
 
-    });
- });
+module.exports = router;
 
- module.exports = router;
+
